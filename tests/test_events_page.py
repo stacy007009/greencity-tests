f265e341dad189d7237f552fdafc1dfd77b3087c
stacy007009 <radevicanastasia24@gmail.com>
2026-04-08 21:09:32 +0300

    Refactor event page tests for clarity and consistency

--- tests/test_events_page.py
+++ tests/test_events_page.py
@@ -4,40 +4,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class TestEventsPage(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 10)
-        self.driver.get("https://www.greencity.cx.ua/#/greenCity/events")
+        self.url = "https://www.greencity.cx.ua/#/greenCity/events"
+        self.driver.get(self.url)
 
     def tearDown(self):
         self.driver.quit()
-      
+        
     def test_page_load(self):
-        body = self.wait.until(
-            EC.presence_of_element_located((By.TAG_NAME, "body"))
-        )
-        self.assertTrue(body.is_displayed())
+        self.assertIn("events", self.driver.current_url)
 
-    def test_events_exist(self):
-        events = self.wait.until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div"))
+    def test_events_cards_present(self):
+        cards = self.wait.until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class,'event')]"))
         )
-        self.assertTrue(len(events) > 0)
+        self.assertTrue(len(cards) > 0)
 
-    def test_open_first_event(self):
-        events = self.wait.until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a"))
-        )
-        events[0].click()
-
-        self.wait.until(
-            EC.url_changes("https://www.greencity.cx.ua/#/greenCity/events")
+    def test_open_event(self):
+        cards = self.wait.until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class,'event')]"))
         )
 
-        self.assertNotEqual(
-            self.driver.current_url,
-            "https://www.greencity.cx.ua/#/greenCity/events"
-        )
+        cards[0].click()
+
+        self.wait.until(EC.url_changes(self.url))
+
+        self.assertNotEqual(self.driver.current_url, self.url)
+
+
+if __name__ == "__main__":
+    unittest.main()
